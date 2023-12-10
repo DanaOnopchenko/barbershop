79acbbd55b3c0eb4967ef422f41d72478a80141a
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2023-12-11 00:47:54 +0200

    added mobile.js

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+(() => { 
+    const menuOpenbtn = document.querySelector('[data-menu-open]');
+    const menuCloseBtn = document.querySelector('[data-menu-close]');
+    const mobileMenu = document.querySelector('[data-menu]');
+    // const body = document.querySelector('body');
+
+    menuOpenbtn.addEventListener('click', openModal);
+    menuCloseBtn.addEventListener('click', openModal);
+
+    function openModal() { 
+        mobileMenu.classList.toggle('is-open');
+        // body.classList.toggle('no-scroll')
+    }
+}
+
+)
