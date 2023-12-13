7a71fdc004a7a97dd898a66e5b541ae61c3007bd
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2023-12-13 02:54:47 +0200

    adaptive portfolio, masters, benefirs, footer

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,14 @@
     const menuOpenbtn = document.querySelector('[data-menu-open]');
     const menuCloseBtn = document.querySelector('[data-menu-close]');
     const mobileMenu = document.querySelector('[data-menu]');
-    // const body = document.querySelector('body');
+    const body = document.querySelector('body');
 
     menuOpenbtn.addEventListener('click', openModal);
     menuCloseBtn.addEventListener('click', openModal);
 
     function openModal() {
         mobileMenu.classList.toggle('is-open');
-        // body.classList.toggle('no-scroll')
-        console.log('click')
+        body.classList.toggle('no-scroll');
+        console.log('click');
     }
 })();
